a4e403bf1695fc8ff4c95b288b0f3a6c15040da9
Blake <44421670+blakel58@users.noreply.github.com>
2024-03-24 00:37:57 -0400

    Bug: server would not close when app closed

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const { app, BrowserWindow } = require('electron');
+const psTree = require('ps-tree');
+const kill = require('tree-kill');
+
+// Get PID of the current process
+const parentPid = process.pid;
+
+// Get all child processes of the current process
+function getChildProcesses(parentPid, callback) {
+  psTree(parentPid, (err, children) => {
+      if (err) {
+          callback(err, null);
+      } else {
+          callback(null, children);
+      }
+  });
+}
 
 function createWindow() {
   // Create the browser window.
-  const win = new BrowserWindow({
+  let win = new BrowserWindow ({
     width: 1000,
     height: 650,
     minWidth: 1000,
@@ -10,14 +26,54 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-    //   devTools: true,
+      //   devTools: true,
     },
   });
 
-  // Load the index.html of the app.
+  // Load index.html
   win.loadFile('index.html');
-//   win.openDevTools();
+
+  win.on('close', (event) => {
+    // Prevent the window from closing immediately
+    event.preventDefault();
+
+    // Function to terminate child processes and perform cleanup
+    function cleanupAndClose() {
+      getChildProcesses(parentPid, (err, children) => {
+        if (err) {
+          // Destroy the window if there's an error
+          win.destroy();
+        } else {
+          const pythonProcesses = children.filter(child => child.COMMAND === 'python.exe');
+          const killPromises = pythonProcesses.map(process => {
+            return new Promise((resolve, reject) => {
+              kill(process.PID, 'SIGTERM', (error) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve();
+                }
+              });
+            });
+          });
+
+          // Wait for all promises to resolve
+          Promise.all(killPromises).then(() => {
+            // Destroy the window
+            win.destroy();
+          }).catch(error => {
+            // Destroy the window
+            win.destroy();
+          });
+        }
+      });
+    }
+
+    // Call the cleanup function
+    cleanupAndClose();
+  });
+
+  // win.openDevTools();
 }
 
 app.whenReady().then(createWindow);
-
